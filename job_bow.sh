bc6ad929b2edd7f52880b69cdab81829fe3ef15b
sharduls007 <sharduls055@gmail.com>
2020-11-23 21:23:26 +0000

    Modified directory

--- job_bow.sh
+++ job_bow.sh
@@ -4,7 +4,7 @@ python3 train.py --batch_sz 16 --gradient_accumulation_steps 40 \
  --data_path './dataset/' \
  --glove_path './dataset/glove.840B.300d.txt' \
  --embed_sz 300 \
- --task mmimdb --task_type classification \
+ --task TrollOpinionMeme --task_type classification \
  --model bow --num_image_embeds 1 --freeze_txt 0 --freeze_img 0  \
  --patience 5 --dropout 0.1 --lr 2e-05 --warmup 0.1 --max_epochs 20 --seed 1 --name bow \
  --hidden_sz 284 --max_seq_len 30
--- job_img.sh
+++ job_img.sh
@@ -3,6 +3,6 @@ python3 train.py --batch_sz 16 --gradient_accumulation_steps 40 \
  --savedir './mmbt/out_dir_img/'\
  --data_path './dataset/' \
  --glove_path './dataset/glove.840B.300d.txt' \
- --task mmimdb --task_type classification \
+ --task TrollOpinionMeme --task_type classification \
  --model img --num_image_embeds 1 --freeze_img 15 \
  --patience 5 --dropout 0.1 --lr 1e-07 --warmup 0.1 --max_epochs 20 --seed 1 --name img
